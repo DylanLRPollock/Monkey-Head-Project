ff761e84ffcd8b625a58f66abd00d4be8e777110
Dylan L.R. Pollock <dylanlrpollock@gmail.com>
2025-06-05 21:20:25 -0400

    Restore executable permissions on scripts

--- repo/pygpt-MHP/docs/build-en.sh
+++ repo/pygpt-MHP/docs/build-en.sh
@@ -1,2 +1,10 @@
+# ==================================================
+# This file is a part of the 'Monkey Head Project'
+# Website:   https://dlrp.ca
+# GitHub:  https://github.com/DylanLRPollock/Monkey-Head-Project
+# License:   https://opensource.org/license/gpl-3-0
+# Overseen By:   Dylan L.R. Pollock
+# Updated:   06.05.2025
+# ==================================================
 make -e SPHINXOPTS="-D language='en'" html
 make latexpdf -e SPHINXOPTS="-D language='en'"
