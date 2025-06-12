ec5721bf0735aac5b594d79d4d0bb8a9033b8aff
Dylan L.R. Pollock <dylanlrpollock@gmail.com>
2025-06-11 22:59:52 -0400

    Add media conversion utilities

--- monkey_head/__init__.py
+++ monkey_head/__init__.py
@@ -14,7 +14,15 @@ from . import subos_manager
 from .logging_setup import configure_logging
 from .utils.logger import get_logger
 from .convert_png_to_jpeg import convert_png_to_jpeg
-from .pdf_pre_digestion import pdf_pre_digestion
+try:
+    from .pdf_pre_digestion import pdf_pre_digestion
+except Exception:  # pragma: no cover - optional dependency
+    pdf_pre_digestion = None
+from .media_conversion import (
+    convert_audio,
+    convert_video,
+    convert_file,
+)
 
 # Initialize project-wide logging as soon as the package is imported
 configure_logging()
@@ -26,4 +34,7 @@ __all__ = [
     "get_logger",
     "convert_png_to_jpeg",
     "pdf_pre_digestion",
+    "convert_audio",
+    "convert_video",
+    "convert_file",
 ]
