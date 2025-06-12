eef8d72e7328b0db03ef9362d3b3416ab5df40ae
Dylan L.R. Pollock <dylanlrpollock@gmail.com>
2025-06-11 22:39:47 -0400

    Add tests for CLI print, logging, CLI run, and PDF conversion

---
+++ tests/test_cli_run.py
@@ -0,0 +1,23 @@
+from monkey_head.cli import CLI
+
+
+class DummyConfig:
+    def __init__(self):
+        self.data = {}
+
+    def set_setting(self, key, value):
+        self.data[key] = value
+
+    def get_setting(self, key, default=None):
+        return self.data.get(key, default)
+
+
+def test_cli_run(monkeypatch, capsys):
+    inputs = iter(["set foo bar", "get foo", "exit"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    cli = CLI()
+    cli.config_manager = DummyConfig()
+    cli.run()
+    output = capsys.readouterr().out.strip().splitlines()
+    assert output == ["bar"]
+    assert cli.config_manager.data["foo"] == "bar"
