7bfc80942d69706444b1303b6c4bb2bbd40e0682
Dylan L.R. Pollock <dylanlrpollock@gmail.com>
2025-06-15 04:35:53 -0400

    Add timestamp formatting to Docker module logs

--- docker/GenCore/GenCore.py
+++ docker/GenCore/GenCore.py
@@ -15,7 +15,10 @@ import logging
 import subprocess
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 logger = logging.getLogger(__name__)
 
 
