b2e3adaeafe81b005023c2a2b63f7d8f672c9c71
Dylan L.R. Pollock <dylanlrpollock@gmail.com>
2026-01-20 20:36:17 -0500

    Remove comments from install batch scripts
    Stripped project and author comments from install.bat and gtk/install.bat. Updated initrd.gz and vmlinuz binaries in both install and gtk directories, possibly reflecting a new build or update.

--- install/gtk/install.bat
+++ install/gtk/install.bat
@@ -1,6 +1,1 @@
-REM Monkey Head Project
-REM By: Dylan L.R. Pollock
-REM www.dlrp.ca
-REM HueyOS: Install batch script (install/gtk)
-
 \tools\loadlin.exe \install\vmlinuz initrd=initrd.gz vga=788
--- install/install.bat
+++ install/install.bat
@@ -1,6 +1,1 @@
-REM Monkey Head Project
-REM By: Dylan L.R. Pollock
-REM www.dlrp.ca
-REM HueyOS: Install batch script (install)
-
 \tools\loadlin.exe vmlinuz initrd=initrd.gz
